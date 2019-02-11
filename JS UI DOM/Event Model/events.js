b05f3f39e6f44b9225a32338af023c49b4ae2801
Tanya Zheleva <tanya.zhelevaa@gmail.com>
2019-02-12 00:30:32 +0200

    Adding event model task

--- JS UI DOM/Event Model/events.js
+++ JS UI DOM/Event Model/events.js
@@ -19,17 +19,30 @@ function solve() {
 
         let found = byId !== null ? byId : byElement;
         let buttons = found.getElementsByClassName('button');
-        let contents = found.getElementsByClassName('content');
-
 
         for (let i = 0; i < buttons.length; i++) {
-            buttons[i].innerHTML = 'hide';
+            buttons[i].textContent = 'hide';
 
-            buttons[i].addEventListener('click', function() {
-                console.log('clicked');
-            });        
+            buttons[i].addEventListener('click', function () {
+                let child = found.childNodes[0];
+
+                while (child !== null) {
+                    if (child.className === 'content') {
+                        if (child.style.display !== 'none') {
+                            child.style.display = 'none';
+                            this.textContent = 'show';
+                        } else {
+                            child.style.display = 'block';
+                            this.textContent = 'hide';
+                        }
+
+                        break;
+                    }
+
+                    child = child.nextSibling;
+                }
+            });
         }
-
     };
 };
 
---
+++ JS UI DOM/jQuery/Task-1/generate.js
@@ -0,0 +1,3 @@
+function createList(selector, count) {
+    
+}
