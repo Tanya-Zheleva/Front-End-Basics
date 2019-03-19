ede478449b6c7c40b1b2bf3f9fa319ccc95c0f42
Tanya-Zheleva <tanya.zhelevaa@gmail.com>
2019-03-19 14:34:08 +0200

    Adding more pads

--- JS UI DOM/Project/main.js
+++ JS UI DOM/Project/main.js
@@ -8,11 +8,38 @@ const paddleHeight = 12;
 const paddleXOffset = width / 2 - paddleWidth / 2;
 const paddleYOffset = height - 30;
 
+const floatingPaddleWidth = 220;
+const floatingPaddleHeight = 15;
+const floatingOffsetX = 150;
+const floatingOffsetY = 200;
+
+const verticalPadWidth = 15;
+const verticalPadHeight = 150;
+const verticalPadXOffset = 30;
+const verticalPadYOffset = 100;
+
 const scoreWidthOffset = 150;
 const scoreHeightOffset = 10;
 
-const ballSize = 25;
+const ballRadius = 25;
 const testVel = 200;
+
+const scoreFont = {
+    size: 38,
+    family: 'New Times Roman',
+    fill: 'white'
+};
+
+function createPaddle(drawer, padWidth, padHeight, x, y, color) {
+    return drawer.rect(padWidth, padHeight)
+        .x(x)
+        .y(y)
+        .fill(color);
+}
+
+function hitsPaddle(ballX, ballY, yStart, yEnd, xStart, xEnd) {
+    return (ballY >= yStart && ballY <= yEnd) && (ballX >= xStart && ballX <= xEnd);
+}
 
 function start() {
     let draw = SVG('drawing').size(width, height);
@@ -22,28 +49,34 @@ function start() {
         .fill('lightgray');
 
     let points = 0;
-    let scoreFont = {
-        size: 38,
-        family: 'New Times Roman',
-        fill: 'white'
-    };
 
     let score = draw.text(`Score: ${points}`).font(scoreFont)
         .move(width - scoreWidthOffset, scoreHeightOffset);
 
-    let paddle = draw.rect(paddleWidth, paddleHeight);
-    paddle.x(paddleXOffset)
-        .y(paddleYOffset)
-        .fill('blue');
+    let paddle = createPaddle(draw, paddleWidth, paddleHeight, paddleXOffset, paddleYOffset, 'blue');
+    let floatingPaddle = createPaddle(draw, floatingPaddleWidth, floatingPaddleHeight, floatingOffsetX, floatingOffsetY, 'green');
+    let leftVerticalPad = createPaddle(draw, verticalPadWidth, verticalPadHeight, verticalPadXOffset, verticalPadYOffset, 'orange');
+    let rightVerticalPad = createPaddle(draw, verticalPadWidth, verticalPadHeight, width - verticalPadXOffset - verticalPadWidth, verticalPadYOffset, 'orange');
 
-    let ball = draw.circle(ballSize);
-    ball.center(width / 2, height / 2)
+    floatingPaddle.animate(3000)
+        .move(600, 200)
+        .loop(true, true);
+
+    leftVerticalPad.animate(3000)
+        .move(30, 400)
+        .loop(true, true);
+
+    rightVerticalPad.animate(2000)
+    .move(955, 400)
+    .loop(true, true);
+
+    let ball = draw.circle(ballRadius)
+        .center(width / 2, height / 2)
         .fill('red');
 
     let velX = testVel;
-    let velY = testVel
-    let paddleXEnd = paddle.x() + paddleWidth;
-    let paddleYEnd = paddle.y() - paddleHeight;
+    let velY = testVel;
+
     let direction = 0; //1 right, -1 left
     let speed = 5;
 
@@ -61,27 +94,30 @@ function start() {
         let ballX = ball.cx();
         let ballY = ball.cy();
 
-        if (velY > 0 && ballY >= paddleYEnd && ballX >= paddle.x() && ballX <= paddleXEnd) { //?
+        let hitsHorizontal = hitsPaddle(ballX, ballY, floatingPaddle.y(), floatingPaddle.y() + floatingPaddleHeight, floatingPaddle.x(), floatingPaddle.x() + floatingPaddleWidth);
+        
+        if (hitsHorizontal) {
             velY = -velY;
         }
 
-        if (velX > 0 && ballX >= paddle.x() && ballX <= paddleXEnd && ballY <= paddle.y() && ballY >= paddleYEnd) { //?
+        let hitsLeft = hitsPaddle(ballX, ballY, leftVerticalPad.y(), leftVerticalPad.y() + verticalPadHeight, leftVerticalPad.x(), leftVerticalPad.x() + verticalPadWidth);
+        let hitsRight = hitsPaddle(ballX, ballY, rightVerticalPad.y(), rightVerticalPad.y() + verticalPadHeight, rightVerticalPad.x(), rightVerticalPad.x() + verticalPadWidth);
+
+        if (hitsLeft || hitsRight) {
             velX = -velX;
         }
 
-        // if (velY < 0 && ballY >= paddleYEnd && ballX >= paddle.x() && ballX <= paddleXEnd) {
-        //     velY = -velY;
-        // }
+        let hitsMain = hitsPaddle(ballX, ballY, paddle.y(), paddle.y() + paddleHeight, paddle.x(), paddle.x() + paddleWidth);
 
-        // if (velY > 0 && ballY >= paddleYEnd && ballX >= paddle.x() && ballX <= paddleXEnd) {
-        //     velY = -velY;
-        // }
+        if (hitsMain) {
+            points++;
+            velY = -velY;
+        }
 
-        // if (velY > 0 && ballY >= height) {
-        //     pauseGame();
-        // }
+        if (velY > 0 && ballY >= height) {
+            pauseGame();
+        }
 
-        //............
         if ((velY < 0 && ballY < 0) || (velY > 0 && ballY >= height)) {
             velY = -velY;
         }
@@ -99,6 +135,8 @@ function start() {
         } else {
             paddle.dx(direction * speed)
         }
+
+        score.text(`Score: ${points}`);
     }
 
     let lastTime;
@@ -119,7 +157,7 @@ function start() {
         velY = 0;
         velX = 0;
 
-        ball.animate(100).center(width / 2, height / 2 - 100);
+        ball.animate(100).center(width / 2, height / 2 - 170);
         paddle.animate(50).cx(width / 2);
     }
 
