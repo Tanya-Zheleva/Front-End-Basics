a90a3e859643d8e888d215ccce23c5dce547e80f
Tanya-Zheleva <tanya.zhelevaa@gmail.com>
2019-03-26 14:35:09 +0200

    Refactoring

--- JS UI DOM/Project/main.js
+++ JS UI DOM/Project/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-//const Utils = require('./constants.js');
-//let util = new Utils();
-
-//console.log(util.width);
-
-// import {test} from './constants';
-// test();
-
 const width = 1000;
 const height = 600;
 
@@ -40,7 +32,10 @@ const scoreWidthOffset = 150;
 const scoreHeightOffset = 10;
 
 const ballRadius = 22;
-const testVel = 200;
+const testVel = 300;
+
+const rightArrowCode = 39;
+const leftArrowCode = 37;
 
 const scoreFont = {
     size: 38,
@@ -49,29 +44,45 @@ const scoreFont = {
 };
 
 function createPaddle(drawer, padWidth, padHeight, x, y, color) {
-    return drawer.rect(padWidth, padHeight)
-        .x(x)
-        .y(y)
-        .fill(color);
+    return {
+        rect: drawer.rect(padWidth, padHeight).x(x).y(y).fill(color),
+        width: padWidth,
+        height: padHeight
+    };
 }
 
-function hitsPaddle(ballX, ballY, yStart, yEnd, xStart, xEnd) {
-    return (ballY >= yStart && ballY <= yEnd) && (ballX >= xStart && ballX <= xEnd);
+function createBall(drawer, x, y, diameter, velX, velY, color) {
+    return {
+        circle: drawer.circle(diameter).center(x, y).fill(color),
+        diameter: diameter,
+        velX: velX,
+        velY: velY
+    };
 }
 
-function hitsTriangle(x, y, coords) {
-    let OA = Math.sqrt((coords.x1 - x) * (coords.x1 - x) + (coords.y1 - y) * (coords.y1 - y));
-    let OB = Math.sqrt((coords.x2 - x) * (coords.x2 - x) + (coords.y2 - y) * (coords.y2 - y));
-    let OC = Math.sqrt((coords.x3 - x) * (coords.x3 - x) + (coords.y3 - y) * (coords.y3 - y));
+function animatePaddle(pad, dx, dy, animationType, duration) {
+    pad.rect.animate(duration, animationType).move(dx, dy).loop(true, true);
+}
 
-    let AB = Math.sqrt((coords.x2 - coords.x1) * (coords.x2 - coords.x1) + (coords.y2 - coords.y1) * (coords.y2 - coords.y1));
-    let CA = Math.sqrt((coords.x1 - coords.x3) * (coords.x1 - coords.x3) + (coords.y1 - coords.y3) * (coords.y1 - coords.y3));
-    let BC = Math.sqrt((coords.x3 - coords.x2) * (coords.x3 - coords.x2) + (coords.y3 - coords.y2) * (coords.y3 - coords.y2));
+function hitsPaddle(ball, pad) {
+    let yStart = pad.rect.y();
+    let yEnd = pad.rect.y() + pad.height;
+    let xStart = pad.rect.x();
+    let xEnd = pad.rect.x() + pad.width;
 
-    console.log((OA + OB + OC));
-    console.log((AB + BC + CA));
+    return (ball.circle.cy() >= yStart && ball.circle.cy() <= yEnd) && (ball.circle.cx() >= xStart && ball.circle.cx() <= xEnd);
+}
 
-    return (OA + OB + OC) < (AB + BC + CA);
+function movePaddle(pad, direction, speed) {
+    let x = pad.rect.x();
+
+    if (x <= 0 && direction == -1) {
+        pad.rect.cx(pad.width / 2);
+    } else if (x >= width - pad.width && direction == 1) {
+        pad.rect.x(width - pad.width);
+    } else {
+        pad.rect.dx(direction * speed)
+    }
 }
 
 function start() {
@@ -92,111 +103,74 @@ function start() {
     let leftTopWall = createPaddle(draw, wallTopWidth, wallTopHeight, wallTopXOffset, wallTopYOffset, 'purple');
     let rightTopWall = createPaddle(draw, wallTopWidth, wallTopHeight, width - wallTopWidth - wallTopXOffset, wallTopYOffset, 'purple');
 
-    floatingPaddle.animate(2000, '<>').move(600, 130).loop(true, true);
-    leftVerticalPad.animate(1500, '<').move(70, 200).loop(true, true);
-    rightVerticalPad.animate(1500, '>').move(910, 200).loop(true, true);
+    animatePaddle(floatingPaddle, 600, 130, '<>', 2000);
+    animatePaddle(leftVerticalPad, 70, 200, '<', 1500);
+    animatePaddle(rightVerticalPad, 910, 200, '>', 1500);
 
-    let ball = draw.circle(ballRadius).center(width / 2, height / 2).fill('red');
+    let ball = createBall(draw, width / 2, height / 2, ballRadius, testVel, testVel, 'red');
     let velX = testVel;
     let velY = testVel;
     let direction = 0; //1 right, -1 left
     let speed = 5;
 
     SVG.on(document, 'keydown', function (e) {
-        direction = e.keyCode == 39 ? 1 : e.keyCode == 37 ? -1 : 0;
+        direction = e.keyCode === rightArrowCode ? 1 : e.keyCode === leftArrowCode ? -1 : 0;
     });
 
     SVG.on(document, 'keyup', function (e) {
         direction = 0;
     });
 
-    // let triangle = draw.polygon('200,0 500,0, 300,100').fill('yellow');
-    // //let triangle2 = draw.polygon('800,0 500,0, 700,100').fill('yellow');
-
-    // let pointsCorrdinates = triangle.array().value;
-
-    // let coords = {
-    //     x1: pointsCorrdinates[0][0],
-    //     y1: pointsCorrdinates[0][1],
-    //     x2: pointsCorrdinates[1][0],
-    //     y2: pointsCorrdinates[1][1],
-    //     x3: pointsCorrdinates[2][0],
-    //     y3: pointsCorrdinates[2][1],
-    // }
-
-    // console.log(coords);
-
     function update(time) {
-        ball.dmove(velX * time, velY * time);
+        ball.circle.dmove(velX * time, velY * time);
 
-        let ballX = ball.cx();
-        let ballY = ball.cy();
+        let hitsLeftWall = hitsPaddle(ball, leftWall);
+        let hitsRightWall = hitsPaddle(ball, rigthWall);
 
-        // let hitsTopTriangle = hitsTriangle(ballX, ballY, coords);
-        // if (hitsTopTriangle) {
-        //     velY = -velY;
-        // }
-
-        let hitsLeftWall = hitsPaddle(ballX, ballY, leftWall.y(), leftWall.y() + wallHeight, leftWall.x(), leftWall.x() + wallWidth);
-        let hitsRightWall = hitsPaddle(ballX, ballY, rigthWall.y(), rigthWall.y() + wallHeight, rigthWall.x(), rigthWall.x() + wallWidth);
-        
         if (hitsLeftWall || hitsRightWall) {
-            velY += 10;
             velY = -velY;
         }
 
-        let hitsLeftTop = hitsPaddle(ballX, ballY, leftTopWall.y(), leftTopWall.y() + wallTopHeight, leftTopWall.x(), leftTopWall.x() + wallTopWidth);
-        let hitsRightTop = hitsPaddle(ballX, ballY, rightTopWall.y(), rightTopWall.y() + wallTopHeight, rightTopWall.x(), rightTopWall.x() + wallTopWidth);
+        let hitsLeftTop = hitsPaddle(ball, leftTopWall);
+        let hitsRightTop = hitsPaddle(ball, rightTopWall);
 
         if (hitsLeftTop || hitsRightTop) {
-            velY += 10;
             velX = -velX;
         }
 
-        let hitsHorizontal = hitsPaddle(ballX, ballY, floatingPaddle.y(), floatingPaddle.y() + floatingHeight, floatingPaddle.x(), floatingPaddle.x() + floatingWidth);
+        let hitsHorizontal = hitsPaddle(ball, floatingPaddle);
 
         if (hitsHorizontal) {
-            velY += 50;
             velY = -velY;
         }
 
-        let hitsLeft = hitsPaddle(ballX, ballY, leftVerticalPad.y(), leftVerticalPad.y() + verticalPadHeight, leftVerticalPad.x(), leftVerticalPad.x() + verticalPadWidth);
-        let hitsRight = hitsPaddle(ballX, ballY, rightVerticalPad.y(), rightVerticalPad.y() + verticalPadHeight, rightVerticalPad.x(), rightVerticalPad.x() + verticalPadWidth);
+        let hitsLeft = hitsPaddle(ball, leftVerticalPad);
+        let hitsRight = hitsPaddle(ball, rightVerticalPad);
 
         if (hitsLeft || hitsRight) {
-            velX += 15;
             velX = -velX;
         }
 
-        let hitsMain = hitsPaddle(ballX, ballY, paddle.y(), paddle.y() + paddleHeight, paddle.x(), paddle.x() + paddleWidth);
+        let hitsMain = hitsPaddle(ball, paddle);
 
         if (hitsMain) {
             points++;
             velY = -velY;
         }
 
-        if (velY > 0 && ballY >= height) {
+        if (velY > 0 && ball.circle.cy() >= height) {
             pauseGame();
         }
 
-        if ((velY < 0 && ballY < 0) || (velY > 0 && ballY >= height)) {
+        if ((velY < 0 && ball.circle.cy() < 0) || (velY > 0 && ball.circle.cy() >= height)) {
             velY = -velY;
         }
 
-        if ((velX < 0 && ballX <= 0) || (velX > 0 && ballX >= width)) {
+        if ((velX < 0 && ball.circle.cx() <= 0) || (velX > 0 && ball.circle.cx() >= width)) {
             velX = -velX;
         }
 
-        let paddleX = paddle.x();
-
-        if (paddleX <= 0 && direction == -1) {
-            paddle.cx(paddleWidth / 2);
-        } else if (paddleX >= width - paddleWidth && direction == 1) {
-            paddle.x(width - paddleWidth);
-        } else {
-            paddle.dx(direction * speed)
-        }
-
+        movePaddle(paddle, direction, speed);
         score.text(`Score: ${points}`);
     }
 
@@ -219,8 +193,8 @@ function start() {
         velX = 0;
         points = 0;
 
-        ball.animate(120).center(width / 2, height / 2 - 260);
-        paddle.animate(50).cx(width / 2);
+        ball.circle.animate(120).center(width / 2, height / 2 - 260);
+        paddle.rect.animate(50).cx(width / 2);
     }
 
     draw.on('click', function () {
