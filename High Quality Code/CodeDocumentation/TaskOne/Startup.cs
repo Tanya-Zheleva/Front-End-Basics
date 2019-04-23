72c8079abae871d1c806d626745286097f7b36d0
Tanya-Zheleva <tanya.zhelevaa@gmail.com>
2019-04-23 13:31:29 +0300

    Adding Code Documentation and Comments homework

---
+++ High Quality Code/CodeDocumentation/TaskOne/Startup.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskOne
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var parsed = StringExtensions.ToValidLatinFileName("test t");
+            Console.WriteLine(parsed);
+        }
+    }
+}
